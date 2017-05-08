adee5e551bd05798fc763b96d9f2f27252488780
Phil Ashby <phlash@ashbysoft.com>
2017-05-08 01:54:31 +0100

    copy ICMP layer

--- pysink.py
+++ pysink.py
@@ -11,6 +11,7 @@
 import sys, getopt
 from scapy.all import *
 
+verb = False
 answering_machines = []
 sink_if = 'eth1'
 sink_ip = '192.168.0.253'
@@ -22,14 +23,14 @@ class ICMP_am:
     self.IP_addr = ip
 
   def is_request(self, p):
-    if (p.haslayer(IP) and
-        p.getlayer(IP).dst == self.IP_addr):
-      return True
+    return (p.haslayer(IP) and
+            p.getlayer(IP).dst == self.IP_addr and
+            p.haslayer(ICMP))
 
   def make_reply(self, p):
     ether = p.getlayer(Ether)
     ip = p.getlayer(IP)
-    icmp = p.getlayer(ICMP)
+    icmp = copy.deepcopy(p.getlayer(ICMP))
     icmp.type = "echo-reply"
     rep = Ether(dst=ether.src)/IP(dst=ip.src, src=ip.dst, proto="icmp")/icmp
     return rep
@@ -47,31 +48,38 @@ def filter(p):
   global answering_machines
   global spin
   global spos
+  global verb
   for am in answering_machines:
     if am.is_request(p):
       r = am.make_reply(p)
       print "\nA: ", p.summary(), "\n\t", r.summary()
       sendp(r, iface=sink_if)
       return
-  print spin[spos], "\r",
-  sys.stdout.flush()
-  spos = (spos+1) % len(spin)
+  if verb:
+    print "\nI: ", p.summary()
+  else:
+    print spin[spos], "\r",
+    sys.stdout.flush()
+    spos = (spos+1) % len(spin)
 
 def bail():
-  print 'pysink.py [-i <sink IF>] [-a <sink IP>]'
+  print 'pysink.py [-h] [-v] [-i <sink IF>] [-a <sink IP>]'
   sys.exit(1)
 
 def main(argv):
   global answering_machines
   global sink_if
   global sink_ip
+  global verb
   try:
-    opts, args = getopt.getopt(argv, "hi:a:")
+    opts, args = getopt.getopt(argv, "hvi:a:")
   except getopt.GetoptError:
     bail()
   for opt, arg in opts:
     if opt == '-h':
       bail()
+    elif opt == '-v':
+      verb = True
     elif opt == '-i':
       sink_if = arg
     elif opt == '-a':
